2e84a9005bfabcb74bc015a26c388c412a1a9251
anandvimal <deepaksandhu789@gmail.com>
2025-07-04 21:16:19 -0700

    intro3 was wrong putting right content now.

--- React Basics/customising-example/src/components/Intro3.js
+++ React Basics/customising-example/src/components/Intro3.js
@@ -1,9 +1,13 @@
-function Footer() {
+function Intro3() {
     return (
-        <div className="copyright">
-            <p>Made with love by Myself</p>
+        <div className="blog-post-intro">
+            <h2>What's the best way to style your React apps?</h2>
+            <div>
+                <p>There are so many options to choose from. Here's a high-level overview of the popular ones.</p>
+                <p className="link">Read more...</p>
+            </div>
         </div>
     );
 };
 
-export default Footer;
+export default Intro3;
