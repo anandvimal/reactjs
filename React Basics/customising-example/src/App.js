578c65c4886cffc4e0baadf6f1b2e936b5c215f3
anandvimal <deepaksandhu789@gmail.com>
2025-07-10 20:25:09 -0700

    focus ref hook

--- React Basics/customising-example/src/App.js
+++ React Basics/customising-example/src/App.js
@@ -20,23 +20,54 @@
 // export default App;
 
 
-import "./App.css";
-import { useState } from "react";
+// import "./App.css";
+// import { useState } from "react";
+
+// function App() {
+//   const [inputValue, setInputValue] = useState("Hello World!");
+
+//   function handleInputChange(event) {
+//     setInputValue(event.target.value);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Task: Add a button and handle a click event</h1>
+//       <input type="text" value={inputValue} onChange={handleInputChange} />
+//       <button onClick={() => setInputValue("Boom reset !")}>
+//         Reset Input
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import "./App.css"; 
+import { useRef } from "react";
 
 function App() {
-  const [inputValue, setInputValue] = useState("Hello World!");
+  const inputRef = useRef(null);
 
-  function handleInputChange(event) {
-    setInputValue(event.target.value);
+  function TextInputWithFocusButton() {
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+      // `current` points to the mounted text input element
+      inputEl.current.focus();
+    };
+    return (
+      <>
+        <input ref={inputEl} type="text" />
+        <button onClick={onButtonClick}>Focus the input</button>
+      </>
+    );
   }
 
   return (
     <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <button onClick={() => setInputValue("Boom reset !")}>
-        Reset Input
-      </button>
+      <TextInputWithFocusButton />
     </div>
   );
 }
