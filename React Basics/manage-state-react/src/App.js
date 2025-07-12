9a7b6310f6fc68e2dcd8b7ead3c0b2348984ae3e
anandvimal <deepaksandhu789@gmail.com>
2025-07-12 14:27:33 -0700

    Update App.js

--- React Basics/manage-state-react/src/App.js
+++ React Basics/manage-state-react/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Where should the state go?</h1>
+      <Fruits />
+      <FruitsCounter />
     </div>
   );
-}
+};
 
 export default App;
