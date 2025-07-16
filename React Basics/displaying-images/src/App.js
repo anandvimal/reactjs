9c3ddd0257b5519c7d09d685dddd9c44e4191852
anandvimal <deepaksandhu789@gmail.com>
2025-07-15 18:45:09 -0700

    Create App.js

---
+++ React Basics/displaying-images/src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/images/lemon.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <img src={logo} alt="lemon" />
+    </div>
+  );
+}
+
+export default App;
